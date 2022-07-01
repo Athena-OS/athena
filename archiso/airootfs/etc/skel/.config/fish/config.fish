b60569eaef8f90993a93df6b74b9ba46dc6b8d92
D3vil0per <>
2022-07-01 03:55:13 +0200

    Edited config.fish for managing unofficial repository keys

--- archiso/airootfs/etc/skel/.config/fish/config.fish
+++ archiso/airootfs/etc/skel/.config/fish/config.fish
@@ -2,3 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 neofetch | lolcat
+
+sudo rm -rf /etc/pacman.d/gnupg
+sudo pacman-key --init
+sudo pacman-key --populate
+
+sed '/pacman/d' $HOME/.config/fish/config.fish
