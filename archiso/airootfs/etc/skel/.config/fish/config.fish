f48a7603768272cb4e586ea8bda8e8595b3d0452
D3vil0per <>
2022-07-06 00:43:16 +0200

    Fixed the managing of blackarch key in Live Env and final system env

--- archiso/airootfs/etc/skel/.config/fish/config.fish
+++ archiso/airootfs/etc/skel/.config/fish/config.fish
@@ -6,6 +6,7 @@ neofetch | lolcat
 sudo rm -rf /etc/pacman.d/gnupg
 sudo pacman-key --init
 sudo pacman-key --populate
+rm -rf /usr/share/pacman/keyrings/blackarch*
 
 sed -i '/pacman/d' $HOME/.config/fish/config.fish
 sed -i '/^$/d' $HOME/.config/fish/config.fish
