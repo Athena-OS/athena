9a422a3a2d95bdd5865a88be529b240f8f6f8347
D3vil0per <>
2022-07-31 06:46:34 +0200

    Update version to 2022.08

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -2,10 +2,10 @@
 # shellcheck disable=SC2034
 
 iso_name="athena"
-iso_label="athena-2022.07"
+iso_label="athena-2022.08"
 iso_publisher="Athena <https://github.com/Athena-OS/athena-iso>"
 iso_application="Athena Live/Rescue CD"
-iso_version="2022.07"
+iso_version="2022.08"
 install_dir="arch"
 buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
