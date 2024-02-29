16c26cb6407f4c59c75faebf8bc3d029fb47f29c
D3vil0p3r <>
2024-02-29 22:47:15 +0100

    Upload pentesting tools

--- packages/pentesting/metasploitavevasion-git/metasploitavevasion.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-LOCAL_CONFIG="${HOME}/.config/metasploitavevasion"
-
-[[ -d "$LOCAL_CONFIG" ]] || install -d "$LOCAL_CONFIG"
-cd "$LOCAL_CONFIG" && avoid.sh
-echo -e "Script complete, press <enter> to exit"; read
