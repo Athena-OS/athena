c39319dbc2260354fc25d05831362a12f61dc688
D3vil0p3r <vozaanthony@gmail.com>
2024-03-23 21:52:47 +0100

    Fix athena-blueteam

--- packages/metapackages/athena-blueteamer/build.sh
+++ packages/metapackages/athena-blueteamer/build.sh
@@ -5,7 +5,7 @@ pkgname=$(grep "^pkgname=" PKGBUILD | awk -F"=" '{print $2}')
 pkgrel=$(grep "^pkgrel=" PKGBUILD | awk -F"=" '{split($2,a," ");gsub(/"/, "", a[1]);print a[1]}')
 arch=$(grep "^arch=" PKGBUILD | awk -F"'" '{print $2}')
 
-updpkgsums
+#updpkgsums
 makepkg -f -scr --noconfirm
 
 pkgver=$(grep "^pkgver=" PKGBUILD | awk -F"=" '{print $2}')
