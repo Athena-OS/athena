231b63d1a5b41693269de1181a04b0686bc7dad8
D3vil0p3r <>
2024-05-12 01:49:48 +0200

    Fix packages

---
+++ packages/libs/python/python-pytorctl/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+   name = "torctl",
+   version = "0.2",
+   description = "A Python library for Tor controllers",
+   license = "BSD",
+   packages=find_packages()
+)
